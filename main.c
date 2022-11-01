9bb93c58c9cf5ce8267ea40271417e83160b8658
rudalstj <jade201913@gmail.com>
2022-11-01 16:36:05 +0900

    02-2

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 
 int main(int argc, char *argv[]) {
 	int grade[SIZE];
-	int i;
-	
-	printf("%d명의 점수를 입력하시오.\n", SIZE);
+	int i, average;
+	int sum = 0;
 	
 	for(i=0; i<SIZE; i++){
+		printf("학생 성적을 입력하시오: ");
 		scanf("%d", &grade[i]);
+		sum += grade[i]; 
 	}
 	
-	for(i=0; i<SIZE; i++){
-		printf("grade[%d] = %d\n", i, grade[i]);
-	}
+	average = sum / SIZE;
+	printf("성적 평균 : %d\n", average);
 		
 	return 0;
 }
